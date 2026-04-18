69974e83aea681effce56a5ede7e3b010b92d08a
0zminDev <thejoycop@proton.com>
2026-04-18 16:49:54 +0200

    Initial commit

---
+++ src/root.zig
@@ -0,0 +1,52 @@
+const std = @import("std");
+
+// App layer
+const Application = struct {
+    const name = "Zig Calculator";
+
+    pub fn init() !void {
+          
+    }
+};   
+
+// UI layer
+const UI = struct {
+    const Self = @This();
+    
+    menu_options: [5][]const u8 = .{"Add", "Sub", "Mul", "Div", "Exit"};
+    app_name: u8 = undefined;
+    
+
+    pub fn init(self: *Self, name: []cosnt u8) !void {
+        self.app_name = name;
+    }
+
+    pub fn draw_title() !void {
+        
+    }
+};
+
+// Calculator class
+const Calcualtor = struct {
+};
+
+pub fn bufferedPrint() !void {
+    // Stdout is for the actual output of your application, for example if you
+    // are implementing gzip, then only the compressed bytes should be sent to
+    // stdout, not any debugging messages.
+    var stdout_buffer: [1024]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+    const stdout = &stdout_writer.interface;
+
+    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+
+    try stdout.flush(); // Don't forget to flush!
+}
+
+pub fn add(a: i32, b: i32) i32 {
+    return a + b;
+}
+
+test "basic add functionality" {
+    try std.testing.expect(add(3, 7) == 10);
+}
